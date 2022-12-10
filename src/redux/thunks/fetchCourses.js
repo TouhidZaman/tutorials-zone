492f63a6cc781fc8d4e38f1b5f257d5aa547e9a0
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-12-10 22:19:01 +0600

    firebase hosting added

--- src/redux/thunks/fetchCourses.js
+++ src/redux/thunks/fetchCourses.js
@@ -1,7 +1,7 @@
 import { loadCourses } from "../actions/courseActions";
 
 const fetchCourses = () => async dispatch => {
-    const response =  await fetch('http://localhost:5000/courses');
+    const response =  await fetch('https://tutorials-zone-server.vercel.app/courses');
     const data = await response.json();
     const courses = data.data;
     if(courses.length) dispatch(loadCourses(courses));
